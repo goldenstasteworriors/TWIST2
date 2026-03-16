4432c8934ece78c4b9682a18988d52aa66f4a703
goldenstasteworriors <2279322250@qq.com>
2026-03-16 16:42:37 +0800

    Make jingchu03 upper-body teleop headless by default

--- teleop_jingchu03_upper_body.sh
+++ teleop_jingchu03_upper_body.sh
@@ -5,7 +5,6 @@ source ~/miniconda3/bin/activate gmr_for_twist2
 cd deploy_real
 
 python xrobot_headset_controller_to_smplx_upper_body.py \
-    --vis \
     --use_redis \
     --redis_ip localhost \
     "$@"
